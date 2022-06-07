f0c6a62bf5f80b38abff5c0df2f876c4040b56fa
Rick Willcox <rickwillcox90@gmail.com>
2022-06-07 21:09:50 +1000

    robots search working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchField: '',
+      robots: robots,
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value.toLowerCase() });
+  };
+
+  render() {
+    const filterRobots = robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />;
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
-import reportWebVitals from './reportWebVitals';
+import CardList from './CardList';
+import App from './App';
+import { robots } from './robots';
 import 'tachyons';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hello />
+    <App />
   </React.StrictMode>,
 );
 
