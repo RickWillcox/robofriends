a9e0920fda9e20f0a9efd176fab3f13c68f93e42
Rick Willcox <rickwillcox90@gmail.com>
2022-06-08 09:41:48 +1000

    convert to hooks / functional components

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,12 +7,20 @@ import './app.css';
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchField, setSearchField] = useState('');
+  const [count, setCount] = useState(0);
 
+  // empty array means this will only run a single time
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => setRobots(users));
+    console.log('This run once');
   }, []);
+
+  // only run if count changes AND at the beginning
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value.toLowerCase());
@@ -28,6 +36,7 @@ function App() {
   return (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
       <SearchBox onSearchChange={onSearchChange} />
       <Scroll>
         <CardList robots={filterRobots} />;
